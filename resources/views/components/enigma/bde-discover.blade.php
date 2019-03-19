c3680c8de426078ef8183a34d42fcd88792bd9d3
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-03-19 18:37:24 +0100

    Removed indications about the need to find us

--- resources/views/components/enigma/bde-discover.blade.php
+++ resources/views/components/enigma/bde-discover.blade.php
@@ -1,8 +1,6 @@
 <p class="description">
     Votre BDE souhaite se présenter. Vous devrez allez les voir chacun à leur tour. Qui sont-ils ?
     <br>
-    Pour chaque étape, vous devez entrer le nom de la personne à deviner, et aller la voir pour obtenir l'énigme suivante.
-    <br>
     La solution de la dernière étape n'est pas le nom de la dernière personne à deviner. Elle vous donnera le code à
     entrer pour valider l'énigme.
 </p>
--- resources/views/components/steps/discover_bde/gael.blade.php
+++ resources/views/components/steps/discover_bde/gael.blade.php
@@ -1,5 +1,5 @@
 <p class="description">
-    C'est le dernier membre que vous n'avez pas deviné, vous n'avez pas vraiment besoin de description, si ?<br>
-    Ses cheveux sont de la même couleur qu'un écran désagréable, quand votre ordinateur plante.<br>
-    Vous devez aller le voir pour obtenir la réponse de cette énigme.
+    C'est le dernier membre que vous n'avez pas deviné, vous n'avez pas vraiment besoin de description, si ?
+    <br>
+    Ses cheveux sont de la même couleur qu'un écran désagréable, quand votre ordinateur plante.
 </p>
